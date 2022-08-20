c7fb29e843fafca95db7e4e740689237335336d8
Leobin93 <leoayala93@gmail.com>
2022-08-20 15:29:12 -0300

    ejercicio complementario ciclos

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+function doWhile(numero) {
+//Funcion incrementa +1 hasta 10 veces
+let contador=10;
+let resultado=0;
+
+do
+{
+  resultado+=1;
+  contador-=1;
+}
+while(contador!==0);
+
+return numero+resultado;
+
+}
+
+
+
+
+
+
